21623074faa6143aba827978c6491d644450330c
surtarso <tarso.galvao@gmail.com>
2025-03-23 07:27:55 -0300

    moved ss help text to main window center with dynamic display of keybinds

--- src/capture/screenshot_manager.h
+++ src/capture/screenshot_manager.h
@@ -12,7 +12,7 @@ public:
     void captureScreenshot(const std::string& windowName, const std::string& outputPath);  // Capture single screenshot
     void captureAllScreenshots(const std::string& tableImage, const std::string& backglassImage, 
                                const std::string& dmdImage, SDL_Window* window);  // Capture all needed screenshots
-
+    std::string keycodeToString(SDL_Keycode key);
 private:
     std::string vpxLogFile;  // Member variable for log path
     std::string shellEscape(const std::string& str);  // Escape shell strings
