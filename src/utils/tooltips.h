1ab1d23096a1d8c0c1627816743be458f8fa7c77
surtarso <tarso.galvao@gmail.com>
2025-03-31 15:05:52 -0300

    completed tooltips...

--- src/utils/tooltips.h
+++ src/utils/tooltips.h
@@ -37,8 +37,23 @@ namespace Tooltips {
             },
 
             // Custom Media
+            {"WheelImage",
+                "Relative path to the wheel image for the table.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
+            {"PuPTopperImage",
+                "Relative path to the PuP topper image.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
             {"TableImage",
                 "Relative path to the table's preview image.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
+            {"PuPTableImage",
+                "Relative path to the PuP table image.\n"
                 "These are relative to your table folder.\n"
                 "/path/to/tables/<table_folder>/"
             },
@@ -47,8 +62,8 @@ namespace Tooltips {
                 "These are relative to your table folder.\n"
                 "/path/to/tables/<table_folder>/"
             },
-            {"WheelImage",
-                "Relative path to the wheel image for the table.\n"
+            {"PuPBackglassImage",
+                "Relative path to the PuP backglass image.\n"
                 "These are relative to your table folder.\n"
                 "/path/to/tables/<table_folder>/"
             },
@@ -57,8 +72,28 @@ namespace Tooltips {
                 "These are relative to your table folder.\n"
                 "/path/to/tables/<table_folder>/"
             },
+            {"PuPDmdImage",
+                "Relative path to the PuP DMD image.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
+            {"PuPFullDmdImage",
+                "Relative path to the PuP full DMD image.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
+            {"PuPTopperVideo",
+                "Relative path to the PuP topper video.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
             {"TableVideo",
                 "Relative path to the table preview video.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
+            {"PuPTableVideo",
+                "Relative path to the PuP table video.\n"
                 "These are relative to your table folder.\n"
                 "/path/to/tables/<table_folder>/"
             },
@@ -67,13 +102,28 @@ namespace Tooltips {
                 "These are relative to your table folder.\n"
                 "/path/to/tables/<table_folder>/"
             },
+            {"PuPBackglassVideo",
+                "Relative path to the PuP backglass video.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
             {"DmdVideo",
                 "Relative path to the DMD video.\n"
                 "These are relative to your table folder.\n"
                 "/path/to/tables/<table_folder>/"
             },
+            {"PuPDmdVideo",
+                "Relative path to the PuP DMD video.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
+            {"PuPFullDmdVideo",
+                "Relative path to the PuP full DMD video.\n"
+                "These are relative to your table folder.\n"
+                "/path/to/tables/<table_folder>/"
+            },
 
-            // Screen/Monitor
+            // WindowSettings
             {"MainMonitor",
                 "Index of the monitor for the table playfield window.\n"
                 "You can use 'xrandr' to get yours."
@@ -100,11 +150,8 @@ namespace Tooltips {
             },
 
             // TitleDisplay
-            {"Path",
+            {"FontPath",
                 "Absolute path to the font file used in the UI."
-            },
-            {"Size",
-                "Font size in points for table title text rendering."
             },
             {"FontColor",
                 "Color of the table title text in R,G,B,A format (0-255).\nExample: 255,255,255,255 for white."
@@ -112,8 +159,11 @@ namespace Tooltips {
             {"FontBgColor",
                 "Background color behind the table title in R,G,B,A format (0-255).\nExample: 0,0,0,128 for semi-transparent black."
             },
+            {"FontSize",
+                "Font size in points for table title text rendering."
+            },
 
-            // Media Dimensions
+            // MediaDimensions
             {"WheelImageSize",
                 "Size of the wheel image in pixels.\n"
                 "This considers a square image."
@@ -133,12 +183,156 @@ namespace Tooltips {
             {"DmdHeight",
                 "Height of the DMD media in pixels."
             },
-            
+
+            // Keybinds
+            {"PreviousTable",
+                "Key to select the previous table in the list."
+            },
+            {"NextTable",
+                "Key to select the next table in the list."
+            },
+            {"FastPrevTable",
+                "Key to quickly jump back 10 tables."
+            },
+            {"FastNextTable",
+                "Key to quickly jump forward 10 tables."
+            },
+            {"JumpNextLetter",
+                "Key to jump to the next table starting with a different letter."
+            },
+            {"JumpPrevLetter",
+                "Key to jump to the previous table starting with a different letter."
+            },
+            {"RandomTable",
+                "Key to jump to a random table."
+            },
+            {"LaunchTable",
+                "Key to launch the selected table."
+            },
+            {"ToggleConfig",
+                "Key to open or close the configuration menu."
+            },
+            {"Quit",
+                "Key to exit menus and application."
+            },
+            {"ConfigSave",
+                "Key to save changes in the configuration menu."
+            },
+            {"ConfigClose",
+                "Key to close the configuration menu without saving."
+            },
+            {"ScreenshotMode",
+                "Key to launch a table in screenshot mode."
+            },
+            {"ScreenshotKey",
+                "Key to take a screenshot while in screenshot mode."
+            },
+            {"ScreenshotQuit",
+                "Key to quit screenshot mode."
+            },
+
             // Internal
             {"SubCmd",
                 "VPinballX internal command to play .vpx tables.\n"
                 "Use VPinballX --help command line menu to see more."
             },
+
+            // DefaultMedia
+            {"DefaultTableImage",
+                "Default relative path to the table image."
+            },
+            {"DefaultPuPTableImage",
+                "Default relative path to the PuP table image."
+            },
+            {"DefaultBackglassImage",
+                "Default relative path to the backglass image."
+            },
+            {"DefaultPuPBackglassImage",
+                "Default relative path to the PuP backglass image."
+            },
+            {"DefaultDmdImage",
+                "Default relative path to the DMD image."
+            },
+            {"DefaultPuPDmdImage",
+                "Default relative path to the PuP DMD image."
+            },
+            {"DefaultPuPFullDmdImage",
+                "Default relative path to the PuP full DMD image."
+            },
+            {"DefaultPupTopperImage",
+                "Default relative path to the PuP topper image."
+            },
+            {"DefaultWheelImage",
+                "Default relative path to the wheel image."
+            },
+            {"DefaultTableVideo",
+                "Default relative path to the table video."
+            },
+            {"DefaultPuPTableVideo",
+                "Default relative path to the PuP table video."
+            },
+            {"DefaultBackglassVideo",
+                "Default relative path to the backglass video."
+            },
+            {"DefaultPuPBackglassVideo",
+                "Default relative path to the PuP backglass video."
+            },
+            {"DefaultDmdVideo",
+                "Default relative path to the DMD video."
+            },
+            {"DefaultPuPDmdVideo",
+                "Default relative path to the PuP DMD video."
+            },
+            {"DefaultPuPFullDmdVideo",
+                "Default relative path to the PuP full DMD video."
+            },
+            {"DefaultPuPTopperVideo",
+                "Default relative path to the PuP topper video."
+            },
+
+            // UISounds
+            {"ScrollPrevSound",
+                "Relative path to the previous scroll sound file."
+            },
+            {"ScrollNextSound",
+                "Relative path to the next scroll sound file."
+            },
+            {"ScrollFastPrevSound",
+                "Relative path to the fast previous scroll sound file."
+            },
+            {"ScrollFastNextSound",
+                "Relative path to the fast next scroll sound file."
+            },
+            {"ScrollJumpPrevSound",
+                "Relative path to the jump previous scroll sound file."
+            },
+            {"ScrollJumpNextSound",
+                "Relative path to the jump next scroll sound file."
+            },
+            {"ScrollRandomSound",
+                "Relative path to the random scroll sound file."
+            },
+            {"LaunchTableSound",
+                "Relative path to the launch table sound file."
+            },
+            {"LaunchScreenshotSound",
+                "Relative path to the launch screenshot sound file."
+            },
+            {"ConfigSaveSound",
+                "Relative path to the config save sound file."
+            },
+            {"ConfigCloseSound",
+                "Relative path to the config close sound file."
+            },
+            {"QuitSound",
+                "Relative path to the quit sound file."
+            },
+            {"ScreenshotTakeSound",
+                "Relative path to the screenshot take sound file."
+            },
+            {"ScreenshotQuitSound",
+                "Relative path to the screenshot quit sound file."
+            }
         };
     }
 }
