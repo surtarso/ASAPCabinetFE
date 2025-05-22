129eed2fe89beb7ec0598169dc578784977aec72
surtarso <tarso.galvao@gmail.com>
2025-05-22 08:54:28 -0300

    added doxygen documentation

--- src/render/table_data.h
+++ src/render/table_data.h
@@ -3,6 +3,39 @@
 
 #include <string>
 
+/**
+ * @brief Structure holding table and media information along with vpxtool metadata.
+ *
+ * This structure encapsulates paths to various media assets related to a table (such as images and videos)
+ * as well as metadata extracted using vpxtool. It is designed to manage the data for a pinball cabinet
+ * rendering front-end.
+ *
+ * Members:
+ *   - title: Filename or title of the table.
+ *   - vpxFile: Full path to the .vpx file associated with the table.
+ *   - folder: Directory containing the .vpx file.
+ *   - playfieldImage: File path to the static table image.
+ *   - wheelImage: File path to the static wheel image.
+ *   - backglassImage: File path to the static backglass image.
+ *   - dmdImage: File path to the static DMD (Dot Matrix Display) image.
+ *   - playfieldVideo: File path to the table video (if available).
+ *   - backglassVideo: File path to the backglass video (if available).
+ *   - dmdVideo: File path to the DMD video (if available).
+ *
+ *   - tableName: Metadata representing the table's name.
+ *   - authorName: Metadata representing the author's name.
+ *   - gameName: Metadata representing the game name.
+ *   - romPath: Metadata representing the ROM path.
+ *   - tableDescription: Metadata with the table's description.
+ *   - tableSaveDate: Metadata capturing when the table was saved.
+ *   - lastModified: Metadata representing the last modification date.
+ *   - releaseDate: Metadata capturing the release date of the table.
+ *   - tableVersion: Metadata indicating the version of the table.
+ *   - tableRevision: Metadata indicating the revision of the table.
+ *
+ *   - manufacturer: Extracted metadata used for sorting, indicating the manufacturer.
+ *   - year: Extracted metadata used for sorting, indicating the year related to the table.
+ */
 struct TableData {
     // Standard paths
     std::string title;           // Filename
