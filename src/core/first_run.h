5d949277e16529655f9c9ed3b5b8c105031fe786
surtarso <tarso.galvao@gmail.com>
2025-05-22 11:41:28 -0300

    added documentation

--- src/core/first_run.h
+++ src/core/first_run.h
@@ -6,6 +6,15 @@
 #include "core/gui_manager.h"
 #include <SDL2/SDL.h>
 
+/**
+ * @brief Executes the initial configuration using the provided configuration service.
+ *
+ * This function reads and applies the configuration settings from the specified file path.
+ *
+ * @param configService Pointer to an IConfigService implementation responsible for configuration management.
+ * @param configPath    The file path to the configuration file to be used for initial setup.
+ * @return bool       True if the initial configuration was successfully executed, false otherwise.
+ */
 bool runInitialConfig(IConfigService* configService, const std::string& configPath);
 
 #endif // FIRST_RUN_H
