6df50a8921cf30701c5af2d8bab09f5d5c03b086
surtarso <tarso.galvao@gmail.com>
2025-11-12 22:14:40 -0300

    fix truncation warning

--- src/editor/editor_first_run.cpp
+++ src/editor/editor_first_run.cpp
@@ -18,9 +18,25 @@ void drawFirstRun(EditorUI& ui) {
         static char vpxPathBuf[1024];
         static bool initialized = false;
 
+        // if (!initialized) {
+        //     strncpy(tablesPathBuf, mutableSettings.VPXTablesPath.c_str(), sizeof(tablesPathBuf));
+        //     strncpy(vpxPathBuf, mutableSettings.VPinballXPath.c_str(), sizeof(vpxPathBuf));
+        //     initialized = true;
+        // }
+
         if (!initialized) {
+            // strncpy copy at most sizeof(tablesPathBuf) characters.
+            // If the source string is >= 1024 characters, it won't be null-terminated.
             strncpy(tablesPathBuf, mutableSettings.VPXTablesPath.c_str(), sizeof(tablesPathBuf));
+
+            // Manually ensure null termination
+            tablesPathBuf[sizeof(tablesPathBuf) - 1] = '\0';
+
             strncpy(vpxPathBuf, mutableSettings.VPinballXPath.c_str(), sizeof(vpxPathBuf));
+
+            // Manually ensure null termination
+            vpxPathBuf[sizeof(vpxPathBuf) - 1] = '\0';
+
             initialized = true;
         }
 
