5c1562893d879f05ed6ac1fca48fa65b63d0b0e5
surtarso <tarso.galvao@gmail.com>
2025-12-03 12:13:33 -0300

    fix magick command

--- src/tables/launchboxdb/lbdb_image.cpp
+++ src/tables/launchboxdb/lbdb_image.cpp
@@ -15,7 +15,7 @@ bool resizeClearLogo(const fs::path& inputPng, int width, int height) {
     // -resize WxH! → force exact dimensions
     // PNG = transparency kept automatically
     std::stringstream cmd;
-    cmd << "magick convert \""
+    cmd << "magick \""
         << inputPng.string()
         << "\" -resize "
         << width << "x" << height << "! "
